4170a5d15decf9514d8506bfc5a3f3d681c83c63
Danyihza <danyahmadihza99@gmail.com>
2020-01-23 16:46:31 +0700

    up

---
+++ index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- My Css -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- Font -->
+    <script src="https://kit.fontawesome.com/db8c543dd4.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+
+    <title>project_apa</title>
+  </head>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">DanyIhza</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Pricing</a>
+            <a class="nav-item nav-link" href="#">Features</a>
+            <a class="nav-item nav-link" href="#">About</a>
+            <a class="nav-item btn btn-primary tombol" href="#">Join Us</a>
+                        
+            </div>
+        </div>
+    </div>
+    </nav>
+
+  <!-- Akhir Navbar -->
+
+
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Get work done <span>faster</span><br> and <span>better</span> with us</h1>
+    <a href="" class="btn btn-primary tombol">Our Work</a>    
+  </div>
+</div>
+  <!-- Akhir Jumbotron -->
+
+  <!-- Container -->
+<div class="container">
+    <!-- Info Panel -->
+    <div class="row justify-content-center">
+        <div class="col-10 info-panel">
+            <div class="row">
+                <div class="col-lg">
+                    <img src="img/employee.png" alt="Employee" class="float-left">
+                    <h4>24 Hours</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div class="col-lg">
+                    <img src="img/hires.png" alt="Hires" class="float-left">
+                    <h4>Hi-Res</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div class="col-lg">
+                    <img src="img/security.png" alt="Security" class="float-left">
+                    <h4>Security</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+            </div>                        
+        </div>
+    </div>
+    <!-- Akhir Info Panel -->
+    
+    <!-- Workingspace -->
+    <div class="row workingspace">
+        <div class="col-lg-6">
+            <img src="img/workingspace.png" alt="Workingspace" class="img-fluid">
+        </div>
+        <div class="col-lg-5">
+            <h3>You <span>work</span> like at <span>home</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga soluta consequuntur laboriosam modi.</p>
+            <a href="" class="btn btn-success tombol">Gallery</a>
+        </div>            
+    </div>
+    <!-- Akhir Workingspace -->
+
+
+
+    <!-- Testi -->
+    <section class="testimonial">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <h5>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui culpa, deleniti sapiente necessitatibus nulla optio"</h5>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Testi -->
+</div>
+<!-- Akhir Container -->
+
+  <body>
+    
+  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
