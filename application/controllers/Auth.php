a998132bf4e483084852413f34784c2b6267ff15
Danyihza <danyahmadihza99@gmail.com>
2020-01-24 00:24:59 +0700

    s

---
+++ application/controllers/Auth.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller 
+{
+    public function index()
+    {
+        # code...
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/login');              
+        $this->load->view('templates/auth_footer');
+    }
+    
+    public function registration()
+    {
+        # code...
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/registration');              
+        $this->load->view('templates/auth_footer');
+    }
+    
+}
