8e86c290171d60e0a1fd93a94042c7e83a84f24f
Danyihza <danyahmadihza99@gmail.com>
2020-01-24 23:00:29 +0700

    sad

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -45,7 +45,13 @@ class Auth extends CI_Controller
                         'role_id' => $user['role_id']
                     ];
                     $this->session->set_userdata($data);
-                    redirect('user');
+                    if ($user['role_id'] == 1) {
+                        # code...
+                        redirect('admin');
+                    } else {
+                        # code...
+                        redirect('user');
+                    }
                 } else {
                     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
                     Wrong Password!</div>');
