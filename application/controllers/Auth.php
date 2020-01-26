63320718c2f43ffd7bd6829c5e970a7a49cc3131
Danyihza <danyahmadihza99@gmail.com>
2020-01-26 18:51:41 +0700

    d

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -121,4 +121,16 @@ class Auth extends CI_Controller
     }
 
 
+    public function blocked() {
+        $data['title'] = 'My Profile';
+        $data['user'] = $this->db->get_where('user', ['email' => 
+        $this->session->userdata('email')])->row_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data); 
+        $this->load->view('auth/blocked');
+        $this->load->view('templates/footer');        
+    }
+
+
 }
