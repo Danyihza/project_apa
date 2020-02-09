948daede451e2e66d03ddd160a80f368dc449179
Danyihza <55189649+Danyihza@users.noreply.github.com>
2020-02-09 18:15:14 +0700

    ftp

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -30,8 +30,8 @@ class User extends CI_Controller
         $data['user'] = $this->db->get_where('user', ['email' =>
         $this->session->userdata('email')])->row_array();
 
-        
-        $this->form_validation->set_rules('name','Full name', 'required|trim');
+
+        $this->form_validation->set_rules('name', 'Full name', 'required|trim');
 
         if ($this->form_validation->run() == false) {
             $this->load->view('templates/header', $data);
@@ -62,9 +62,9 @@ class User extends CI_Controller
                         # code...
                         unlink(FCPATH . 'assets/img/profile/' . $old_image);
                     }
-                    
+
                     $new_image = $this->upload->data('file_name');
-                    $this->db->set('image',$new_image);
+                    $this->db->set('image', $new_image);
                 } else {
                     echo $this->upload->display_errors();
                 }
@@ -76,6 +76,47 @@ class User extends CI_Controller
 
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your profile has been updated</div>');
             redirect('user');
+        }
+    }
+
+    public function changePassword()
+    {
+        $data['title'] = 'Change Password';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('current_password', 'Current Password', 'required|trim');
+        $this->form_validation->set_rules('new_password1', 'New Password', 'required|trim|min_length[3]|matches[new_password2]');
+        $this->form_validation->set_rules('new_password2', 'Confirm New Password', 'required|trim|min_length[3]|matches[new_password1]');
+
+        if ($this->form_validation->run() == false) {
+            # code...
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/changepassword', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $current_password = $this->input->post('current_password');
+            $new_password = $this->input->post('new_password1');
+            if (!password_verify($current_password, $data['user']['password'])) {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Current Password</div>');
+                redirect('user/changepassword');
+            } else {
+                if ($current_password == $new_password) {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">New Password Can not be the same</div>');
+                    redirect('user/changepassword');
+                } else {
+                    //password sudah ok
+                    $password_hash = password_hash ($new_password, PASSWORD_DEFAULT);
+                    
+                    $this->db->set('password', $password_hash);
+                    $this->db->where('email', $this->session->userdata('email'));
+                    $this->db->update('user');
+                    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password Changed</div>');
+                    redirect('user/changepassword');
+                }
+            }
         }
     }
 }
